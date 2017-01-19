9b9b96e4825f96343695c2db4252b99aeb51f56d
Gabriel Bull <me@gabrielbull.com>
2017-01-19 14:18:05 -0500

    Updated dependencies

--- test/tests/components/module/includes/webpack.config.js
+++ test/tests/components/module/includes/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015', 'stage-0', 'react'],
           plugins: []
--- test/tests/stats/includes/webpack.config.js
+++ test/tests/stats/includes/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015', 'stage-0', 'react'],
           plugins: []
